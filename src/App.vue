37743990f655f120ab4716445252d913e1848a73
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-10-01 23:55:42 +0700

    implement PriceFormat component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <price-format :price="1000000000000"></price-format>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PriceFormat from './components/PriceFormat.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PriceFormat
   }
 }
 </script>
---
+++ src/components/PriceFormat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ price | numberWithCommas }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PriceFormat',
+  props: ['price'],
+  filters: {
+    numberWithCommas (number) {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
+  }
+}
+</script>
+
